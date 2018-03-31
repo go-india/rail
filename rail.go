a427c9df316b97a14e3344e5e3bce682922cdc72
Yash Raj Singh <mail.yashrajsingh@gmail.com>
2018-03-31 20:43:09 +0530

    refactored common usage

--- rail.go
+++ rail.go
@@ -2,7 +2,6 @@ package rail
 
 import (
 	"encoding/json"
-	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -116,60 +115,6 @@ type Train struct {
 	Days    []Day   `json:"days"`
 }
 
-// ExtendedTrain holds extended train details
-type ExtendedTrain struct {
-	Train
-
-	StationTo          Station       `json:"to_station"`
-	StationFrom        Station       `json:"from_station"`
-	SourceDeparture    time.Time     // `json:"src_departure_time"`
-	DestinationArrival time.Time     // `json:"dest_arrival_time"`
-	TravelDuration     time.Duration // `json:"travel_time"`
-}
-
-// UnmarshalJSON convert JSON data to struct
-func (et *ExtendedTrain) UnmarshalJSON(data []byte) error {
-	type Alias ExtendedTrain
-	t := struct {
-		Alias
-		SourceDepartureTime    string `json:"src_departure_time"`
-		DestinationArrivalTime string `json:"dest_arrival_time"`
-		TravelTime             string `json:"travel_time"`
-	}{}
-	if err := json.Unmarshal(data, &t); err != nil {
-		return errors.Wrap(err, "UnmarshalJSON failed")
-	}
-
-	*et = ExtendedTrain(t.Alias)
-
-	if len(t.SourceDepartureTime) == 5 {
-		sdt, err := time.Parse("15:04", t.SourceDepartureTime)
-		if err != nil {
-			return errors.Wrap(err, "parse SourceDepartureTime failed")
-		}
-		et.SourceDeparture = sdt
-	}
-
-	if len(t.DestinationArrivalTime) == 5 {
-		dat, err := time.Parse("15:04", t.DestinationArrivalTime)
-		if err != nil {
-			return errors.Wrap(err, "parse DestinationArrivalTime failed")
-		}
-		et.DestinationArrival = dat
-	}
-
-	if len(t.TravelTime) == 5 {
-		d := func(s string) string { return strings.Replace(s, ":", "h", -1) + "m" }
-		dur, err := time.ParseDuration(d(t.TravelTime))
-		if err != nil {
-			return errors.Wrap(err, "parse travelTime failed")
-		}
-		et.TravelDuration = dur
-	}
-
-	return nil
-}
-
 // Passenger holds passenger details
 type Passenger struct {
 	Number        int    `json:"no"`
@@ -183,89 +128,6 @@ type Station struct {
 	Longitude float64 `json:"lng"`
 	Name      string  `json:"name"`
 	Code      string  `json:"code"`
-}
-
-// TrainWithTimings holds train timings
-type TrainWithTimings struct {
-	Train
-
-	DelayArrival   time.Time //`json:"delayarr"`
-	DelayDeparture time.Time //`json:"delaydep"`
-
-	ScheduledArrival   time.Time //`json:"scharr"`
-	ScheduledDeparture time.Time //`json:"schdep"`
-	ActualDeparture    time.Time //`json:"actdep"`
-	ActualArrival      time.Time //`json:"actarr"`
-}
-
-// UnmarshalJSON convert JSON data to struct
-func (r *TrainWithTimings) UnmarshalJSON(data []byte) error {
-	t := struct {
-		ScheduledArrival   string `json:"scharr"`
-		ScheduledDeparture string `json:"schdep"`
-		ActualDeparture    string `json:"actdep"`
-		ActualArrival      string `json:"actarr"`
-
-		DelayArrival   string `json:"delayarr"`
-		DelayDeparture string `json:"delaydep"`
-
-		Train
-	}{}
-	if err := json.Unmarshal(data, &t); err != nil {
-		return errors.Wrap(err, "UnmarshalJSON failed")
-	}
-
-	r.Train = t.Train
-
-	if len(t.ScheduledArrival) == 5 {
-		sa, err := time.Parse("15:04", t.ScheduledArrival)
-		if err != nil {
-			return errors.Wrap(err, "parse ScheduledArrival failed")
-		}
-		r.ScheduledArrival = sa
-	}
-
-	if len(t.ScheduledDeparture) == 5 {
-		sd, err := time.Parse("15:04", t.ScheduledDeparture)
-		if err != nil {
-			return errors.Wrap(err, "parse ScheduledDeparture failed")
-		}
-		r.ScheduledDeparture = sd
-	}
-
-	if len(t.ActualDeparture) == 5 {
-		ad, err := time.Parse("15:04", t.ActualDeparture)
-		if err != nil {
-			return errors.Wrap(err, "parse ActualDeparture failed")
-		}
-		r.ActualDeparture = ad
-	}
-
-	if len(t.ActualArrival) == 5 {
-		aa, err := time.Parse("15:04", t.ActualArrival)
-		if err != nil {
-			return errors.Wrap(err, "parse ActualArrival failed")
-		}
-		r.ActualArrival = aa
-	}
-
-	if len(t.DelayArrival) == 5 {
-		da, err := time.Parse("15:04", t.DelayArrival)
-		if err != nil {
-			return errors.Wrap(err, "parse DelayArrival failed")
-		}
-		r.DelayArrival = da
-	}
-
-	if len(t.DelayDeparture) == 5 {
-		dd, err := time.Parse("15:04", t.DelayDeparture)
-		if err != nil {
-			return errors.Wrap(err, "parse DelayDeparture failed")
-		}
-		r.DelayDeparture = dd
-	}
-
-	return nil
 }
 
 // Route holds route details
--- station.go
+++ station.go
@@ -1,8 +1,10 @@
 package rail
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -34,6 +36,60 @@ func (r TrainBetweenStationsReq) Request() (*http.Request, error) {
 	)
 
 	return http.NewRequest(http.MethodGet, urlStr, nil)
+}
+
+// ExtendedTrain holds extended train details
+type ExtendedTrain struct {
+	Train
+
+	StationTo          Station       `json:"to_station"`
+	StationFrom        Station       `json:"from_station"`
+	SourceDeparture    time.Time     // `json:"src_departure_time"`
+	DestinationArrival time.Time     // `json:"dest_arrival_time"`
+	TravelDuration     time.Duration // `json:"travel_time"`
+}
+
+// UnmarshalJSON convert JSON data to struct
+func (et *ExtendedTrain) UnmarshalJSON(data []byte) error {
+	type Alias ExtendedTrain
+	t := struct {
+		Alias
+		SourceDepartureTime    string `json:"src_departure_time"`
+		DestinationArrivalTime string `json:"dest_arrival_time"`
+		TravelTime             string `json:"travel_time"`
+	}{}
+	if err := json.Unmarshal(data, &t); err != nil {
+		return errors.Wrap(err, "UnmarshalJSON failed")
+	}
+
+	*et = ExtendedTrain(t.Alias)
+
+	if len(t.SourceDepartureTime) == 5 {
+		sdt, err := time.Parse("15:04", t.SourceDepartureTime)
+		if err != nil {
+			return errors.Wrap(err, "parse SourceDepartureTime failed")
+		}
+		et.SourceDeparture = sdt
+	}
+
+	if len(t.DestinationArrivalTime) == 5 {
+		dat, err := time.Parse("15:04", t.DestinationArrivalTime)
+		if err != nil {
+			return errors.Wrap(err, "parse DestinationArrivalTime failed")
+		}
+		et.DestinationArrival = dat
+	}
+
+	if len(t.TravelTime) == 5 {
+		d := func(s string) string { return strings.Replace(s, ":", "h", -1) + "m" }
+		dur, err := time.ParseDuration(d(t.TravelTime))
+		if err != nil {
+			return errors.Wrap(err, "parse travelTime failed")
+		}
+		et.TravelDuration = dur
+	}
+
+	return nil
 }
 
 // TrainBetweenStationsResp holds trains between stations
@@ -76,6 +132,89 @@ func (r TrainArrivalsReq) Request() (*http.Request, error) {
 	urlStr += fmt.Sprintf("/station/%s/hours/%d", r.Station, hours)
 
 	return http.NewRequest(http.MethodGet, urlStr, nil)
+}
+
+// TrainWithTimings holds train timings
+type TrainWithTimings struct {
+	Train
+
+	DelayArrival   time.Time //`json:"delayarr"`
+	DelayDeparture time.Time //`json:"delaydep"`
+
+	ScheduledArrival   time.Time //`json:"scharr"`
+	ScheduledDeparture time.Time //`json:"schdep"`
+	ActualDeparture    time.Time //`json:"actdep"`
+	ActualArrival      time.Time //`json:"actarr"`
+}
+
+// UnmarshalJSON convert JSON data to struct
+func (r *TrainWithTimings) UnmarshalJSON(data []byte) error {
+	t := struct {
+		ScheduledArrival   string `json:"scharr"`
+		ScheduledDeparture string `json:"schdep"`
+		ActualDeparture    string `json:"actdep"`
+		ActualArrival      string `json:"actarr"`
+
+		DelayArrival   string `json:"delayarr"`
+		DelayDeparture string `json:"delaydep"`
+
+		Train
+	}{}
+	if err := json.Unmarshal(data, &t); err != nil {
+		return errors.Wrap(err, "UnmarshalJSON failed")
+	}
+
+	r.Train = t.Train
+
+	if len(t.ScheduledArrival) == 5 {
+		sa, err := time.Parse("15:04", t.ScheduledArrival)
+		if err != nil {
+			return errors.Wrap(err, "parse ScheduledArrival failed")
+		}
+		r.ScheduledArrival = sa
+	}
+
+	if len(t.ScheduledDeparture) == 5 {
+		sd, err := time.Parse("15:04", t.ScheduledDeparture)
+		if err != nil {
+			return errors.Wrap(err, "parse ScheduledDeparture failed")
+		}
+		r.ScheduledDeparture = sd
+	}
+
+	if len(t.ActualDeparture) == 5 {
+		ad, err := time.Parse("15:04", t.ActualDeparture)
+		if err != nil {
+			return errors.Wrap(err, "parse ActualDeparture failed")
+		}
+		r.ActualDeparture = ad
+	}
+
+	if len(t.ActualArrival) == 5 {
+		aa, err := time.Parse("15:04", t.ActualArrival)
+		if err != nil {
+			return errors.Wrap(err, "parse ActualArrival failed")
+		}
+		r.ActualArrival = aa
+	}
+
+	if len(t.DelayArrival) == 5 {
+		da, err := time.Parse("15:04", t.DelayArrival)
+		if err != nil {
+			return errors.Wrap(err, "parse DelayArrival failed")
+		}
+		r.DelayArrival = da
+	}
+
+	if len(t.DelayDeparture) == 5 {
+		dd, err := time.Parse("15:04", t.DelayDeparture)
+		if err != nil {
+			return errors.Wrap(err, "parse DelayDeparture failed")
+		}
+		r.DelayDeparture = dd
+	}
+
+	return nil
 }
 
 // TrainArrivalsResp holds train arrivals details
